6fc95b7f021b08e2d91758428938d0609d735873
Tomer Noy <tomernoy1@gmail.com>
2020-02-14 20:07:47 +0200

    add ping to pvwa

--- TestAws/Class1.cs
+++ TestAws/Class1.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using System;
+using System.Net.NetworkInformation;
 
 namespace TestAws
 {
@@ -8,7 +10,14 @@ namespace TestAws
         [Category("sanity")]
         public void Test1()
         {
-            Assert.True(true);
+            var url = TestContext.Parameters.Get("url", "");
+            Console.WriteLine($"PVWA url is {url}");
+            Assert.True(!String.IsNullOrEmpty(url));
+            var pinger = new Ping();
+            PingReply reply = pinger.Send(url);
+            var pingable = reply.Status == IPStatus.Success;
+            Console.WriteLine($"PVWA url is {url}");
+            Assert.True(pingable, "check if pingable");
         }
     }
 }
